0cbf63f75bc1389a628dd196cad6a1927c23b00b
c20xh2 <c20xh2@gmail.com>
2018-06-05 20:32:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
 from lib import pihole_object
 from time import sleep
 from sys import argv
+import argparse
 
-
-def get_line_numbers():
-	z = 0
-	line = None
-	with open('WebPassword.txt', 'r') as passfile:
-		for line in passfile:
-			if '###' in line:
-				pass
-			else:
-				line = line.replace('WEBPASSWORD', '').replace("'", '').replace('=','').strip()
-				z+=1
-	return z, line
-
-
-
-
-def main(webpass):
-	pihole_obj = pihole_object.pihole_obj(webpass.strip())
+def main(webpassword, server_ip):
+	pihole_obj = pihole_object.pihole_obj(webpassword, server_ip)
 	while True:
 		pihole_obj.update()
 		pihole_obj.print_stats()
 		sleep(1)
 
 
-z, webpass = get_line_numbers()
+parser = argparse.ArgumentParser()
 
-if z != 1:
-	if len(argv) != 2:
-		print('\n\nError, please provide the WEBPASSWORD (found via cat /etc/pihole/setupVars.conf)\n')
-	else:
-		webpass = str(argv[1])
-		main(webpass)
+parser.add_argument('-w', '--webpassword', help="WEBPASSWORD (found via cat /etc/pihole/setupVars.conf)", default = None)
+parser.add_argument('-i', '--ip', help="IP address of the Pi-Hole server", default = '127.0.0.1')
 
-else:
-	main(webpass)
+args = parser.parse_args()
+
+webpassword = args.webpassword
+server_ip = args.ip
+
+if args.webpassword is None:
+	with open('WebPassword.txt', 'r') as passfile:
+		for line in passfile:
+			if 'WEBPASSWORD' in line:
+				webpassword = line.replace('WEBPASSWORD', '').replace("'", '').replace('=','').strip()
+
+main(webpassword, server_ip)
